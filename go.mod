158b309f422ae12f4965104ab6562532689c01cd
Nimesh Srivastava <snimesh412@gmail.com>
2021-10-09 01:27:33 +0530

    Initialize mongo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module Instagram-backend-api
+
+go 1.17
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type User struct {
+	id    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	name  string             `json:"name,omitempty" bson:"name,omitempty"`
+	email string             `json:"email,omitempty" bson:"email,omitempty"`
+	pswd  string             `json:"pswd,omitempty" bson:"pswd,omitempty"`
 }
+
+var client *mongo.Client
 
 func main() {
 	fmt.Println("Starting the app...")
