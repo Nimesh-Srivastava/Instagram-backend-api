3664611e179561540a3cccadecb56cb9821d2519
Nimesh Srivastava <snimesh412@gmail.com>
2021-10-09 03:29:25 +0530

    Removed warnings

--- main.go
+++ main.go
@@ -8,45 +8,54 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type User struct {
-	id    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	name  string             `json:"name,omitempty" bson:"name,omitempty"`
-	email string             `json:"email,omitempty" bson:"email,omitempty"`
-	pswd  string             `json:"pswd,omitempty" bson:"pswd,omitempty"`
+	Id    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email string             `json:"email,omitempty" bson:"email,omitempty"`
+	Pswd  string             `json:"pswd,omitempty" bson:"pswd,omitempty"`
 }
 
 var client *mongo.Client
 
-// func initialize(w http.ResponseWriter, r *http.Request) {}
-
 func createUser(response http.ResponseWriter, request *http.Request) {
+
 	response.Header().Add("content-type", "application/json")
 
-	var user User
-	json.NewDecoder(request.Body).Decode(&user)
+	var U User
+	json.NewDecoder(request.Body).Decode(&U)
 
 	collection := client.Database("instagram").Collection("users")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	result, _ := collection.InsertOne(ctx, user)
+	ctx, err_ctx := context.WithTimeout(context.Background(), 10*time.Second)
+	if err_ctx != nil {
+		log.Fatal("ListenAndServe: ", err_ctx)
+	}
+	result, _ := collection.InsertOne(ctx, U)
 	json.NewEncoder(response).Encode(result)
 }
 
 func main() {
-	fmt.Println("Starting the app...")
+	fmt.Println("App started...")
 
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, err_ctx := context.WithTimeout(context.Background(), 10*time.Second)
+	if err_ctx != nil {
+		log.Fatal("ListenAndServe: ", err_ctx)
+	}
 	// client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 
 	//Set router
-	http.HandleFunc("/user", createUser)
+	router := mux.NewRouter()
 
-	err := http.ListenAndServe(":9090", nil)
+	//Create a user
+	router.HandleFunc("/user", createUser).Methods("POST")
+
+	err := http.ListenAndServe(":1234", router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
