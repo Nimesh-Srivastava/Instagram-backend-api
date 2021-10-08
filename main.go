d6598b16a3e1b4db3f4052cd982359883ad692a0
Nimesh Srivastava <snimesh412@gmail.com>
2021-10-09 02:11:44 +0530

    Update

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,8 +22,18 @@ type User struct {
 
 var client *mongo.Client
 
-func initialize(w http.ResponseWriter, r *http.Request) {
+// func initialize(w http.ResponseWriter, r *http.Request) {}
 
+func createUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Add("content-type", "application/json")
+
+	var user User
+	json.NewDecoder(request.Body).Decode(&user)
+
+	collection := client.Database("instagram").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
+	json.NewEncoder(response).Encode(result)
 }
 
 func main() {
@@ -33,7 +44,7 @@ func main() {
 	mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 
 	//Set router
-	http.HandleFunc("/", initialize)
+	http.HandleFunc("/user", createUser)
 
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
