366368da18273c47708c0ef0c7c814305f63bdf0
Nimesh Srivastava <snimesh412@gmail.com>
2021-10-09 01:51:48 +0530

    update

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type User struct {
@@ -18,4 +23,15 @@ var client *mongo.Client
 
 func main() {
 	fmt.Println("Starting the app...")
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	//Set router
+	http.HandleFunc("/", nil)
+
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
