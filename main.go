46502543c812c86bc7ddb3184495ad2d0dfe4932
Nimesh Srivastava <snimesh412@gmail.com>
2021-10-09 15:18:51 +0530

    Added hash password feature

--- main.go
+++ main.go
@@ -8,64 +8,118 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
+var client *mongo.Client
+
+//New user structure
 type User struct {
-	Id    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Name  string             `json:"name,omitempty" bson:"name,omitempty"`
-	Email string             `json:"email,omitempty" bson:"email,omitempty"`
-	Pswd  string             `json:"pswd,omitempty" bson:"pswd,omitempty"`
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name     string             `json:"name,omitempty" bson:"name,omitempty"`
+	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" bson:"password,omitempty"`
 }
 
-type Post struct {
-	Id        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+//New post structure
+type PostPic struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Caption   string             `json:"caption,omitempty" bson:"caption,omitempty"`
 	Imgurl    string             `json:"imgurl,omitempty" bson:"imgurl,omitempty"`
 	Timestamp string             `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
 }
 
-var client *mongo.Client
+//Convert string password to hash value
+func getHash(pwd []byte) string {
+	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+	if err != nil {
+		fmt.Println("Password conversion error : ", err)
+	}
+	return string(hash)
+}
 
-func createUser(response http.ResponseWriter, request *http.Request) {
+func CreateUser(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
 
-	response.Header().Add("content-type", "application/json")
+	var user User
+	_ = json.NewDecoder(request.Body).Decode(&user)
 
-	var U User
-	json.NewDecoder(request.Body).Decode(&U)
+	//Securely store password
+	user.Password = getHash([]byte(user.Password))
 
+	//DB name : instagram, collection name : accounts
 	collection := client.Database("instagram").Collection("users")
 
-	ctx := context.Background()
-	ctx, _ = context.WithTimeout(ctx, 5*time.Second)
-	// if err_ctx != nil {
-	// 	log.Fatal("context_error_createUser: ", err_ctx)
-	// }
-
-	result, _ := collection.InsertOne(ctx, U)
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := collection.InsertOne(ctx, user)
 	json.NewEncoder(response).Encode(result)
+}
+
+func GetPeopleEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	params := mux.Vars(request)
+	id, _ := primitive.ObjectIDFromHex(params["id"])
+	var person User
+	collection := client.Database("instagram").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	err := collection.FindOne(ctx, User{ID: id}).Decode(&person)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(person)
+}
+
+func GetPersonEndpoint(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var people []User
+	collection := client.Database("instagram").Collection("users")
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var user User
+		cursor.Decode(&user)
+		people = append(people, user)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(people)
 }
 
 func main() {
 	fmt.Println("App started...")
 
-	ctx := context.Background()
-	ctx, _ = context.WithTimeout(ctx, 3*time.Second)
-	// if err != nil {
-	// 	log.Fatal("context_error_main: ", err)
-	// }
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, _ := mongo.Connect(ctx, clientOptions)
-	_ = client
+	client, _ = mongo.Connect(ctx, clientOptions)
 
-	//Set router
+	//Define router
 	router := mux.NewRouter()
 
-	//Create a user
-	router.HandleFunc("/user", createUser).Methods("POST")
+	//Add a new user
+	router.HandleFunc("/users", CreateUser).Methods("POST")
 
+	//
+	router.HandleFunc("/accounts", GetPeopleEndpoint).Methods("GET")
+
+	//
+	router.HandleFunc("/users/{id}", GetPersonEndpoint).Methods("GET")
+
+	//Start http server on port 9090
 	http.ListenAndServe(":9090", router)
 }
